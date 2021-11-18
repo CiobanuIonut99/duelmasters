3160b8948bc4165ed4c8c91aeb65415db944f011
Ciprian <steffanciprian@gmail.com>
2021-11-18 13:46:04 +0200

    deleted ","

--- src/main/resources/liquibase.scripts/0003_create_table_player.sql
+++ src/main/resources/liquibase.scripts/0003_create_table_player.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS duel_masters.player(
     ID SERIAL unique,
     USERNAME character varying (255),
     CARD_ID_DECK integer,
-    CONSTRAINT CARD_ID_DECK FOREIGN KEY (CARD_ID_DECK) REFERENCES duel_masters.CARD (ID),
+    CONSTRAINT CARD_ID_DECK FOREIGN KEY (CARD_ID_DECK) REFERENCES duel_masters.CARD (ID)
 );
