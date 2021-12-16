d1a540782490580dc36f92aa7eb388103c1ede3f
Ciprian_Stefan <Ciprian.Stefan@Dellteam.com>
2021-12-16 11:33:15 +0200

    vreau sa testez ceva

--- src/main/java/com/cia/duelmasters/DuelmastersApplication.java
+++ src/main/java/com/cia/duelmasters/DuelmastersApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DuelmastersApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DuelmastersApplication.class, args);
 	}
 
